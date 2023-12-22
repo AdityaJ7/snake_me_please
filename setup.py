6a2d518b673ea31353a6354547005da69ccb267c
Aditya Jetely <154300412+aditya-jetely-07@users.noreply.github.com>
2023-12-22 10:09:24 +0530

    chore(setup.py): Long description
    Used readme file to add long description to the package

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 # snake_me_please/setup.py
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="snake_me_please",
     description="Snake Me Please is a Python package and command-line tool designed to convert user-defined variable names to snake_case in Python files while excluding imported variables and keywords from Python libraries.",
-    version="0.2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.3",
     author="Aditya Jetely",
     author_email="ajetely@gmail.com",
     license="public",
