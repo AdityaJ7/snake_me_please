71d6d0dd389c64273aeaec2ba00b0b346ab5a1fd
Aditya Jetely <154300412+aditya-jetely-07@users.noreply.github.com>
2023-12-22 10:01:26 +0530

    chore(setup.py): Author and description added
    Author and description added to the setup.py of the package

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="snake_me_please",
-    version="0.1",
+    description="Snake Me Please is a Python package and command-line tool designed to convert user-defined variable names to snake_case in Python files while excluding imported variables and keywords from Python libraries.",
+    version="0.2",
+    author="Aditya Jetely",
+    author_email="ajetely@gmail.com",
+    license="public",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
